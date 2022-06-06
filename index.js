b2e64c8f753e0d70c22ef91ecd748cbbbc55eede
Spencer Mathews <smathews0906@gmail.com>
2022-06-06 12:48:34 -0500

    commit tree fe0941bb43fce73a19417b424f4c85cec7e3347b

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity='go to the office'){
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(y="*"){
+    return function(x='special'){
+        return(`You are ${y}${x}${y}!`);
+    }
+}
